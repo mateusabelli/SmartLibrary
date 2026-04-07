3b684f2a0444fd88039ec36eaf90ab9726730478
Mateus Abelli <mateusabelli@gmail.com>
2026-04-07 16:12:17 -0300

    Fix required field

--- LendingService/Dtos/LendCreateDto.cs
+++ LendingService/Dtos/LendCreateDto.cs
@@ -2,7 +2,7 @@ namespace LendingService.Dtos;
 
 public class LendCreateDto
 {
-    public int BookId { get; set; }
+    public required int BookId { get; set; }
 
     public required string Borrower { get; set; }
 }
